3317df0611c263be9c36a30ea39fbb75d948c2d4
Noktoborus <noktoborus@gmail.com>
2013-01-01 00:45:02 +0400

    lex preparing

--- milex.c
+++ milex.c
@@ -4,11 +4,28 @@
 #include "milex.h"
 
 void
-milex_next (struct milex *lex, char *buffer, size_t bfsz)
+milex_parse (struct milex *lex)
 {
+	/* TODO */
+}
+
+void
+milex_next (struct milex *restrict lex, const char *restrict buffer, size_t bfsz)
+{
+	register size_t i = 0;
 	if (!lex || !buffer)
 		return;
-	if (bfsz == 0 && (lex->state == MILEX_PROC)
-		lex->state = MILEX_OK;
+	if (bfsz == 0 && (lex->state == MILEX_PROC))
+		lex->state = MILEX_EOL | MILEX_OK;
+	for (; i < bfsz; i ++)
+	{
+		if (buffer[i] == '\n' || buffer[i] == '\r' || buffer[i] == ' ')
+		{
+			milex_parse (lex);
+			lex->state = MILEX_OK;
+			if (buffer[i] != ' ')
+				lex->state |= MILEX_EOL;
+		}
+	}
 }
 
--- milex.h
+++ milex.h
@@ -4,12 +4,14 @@
 #ifndef _MILEX_1356959206_H_
 #define _MILEX_1356959206_H_
 
-#define MILEX_OK   0x0
+#define MILEX_PROC 0x0
 #define MILEX_FAIL 0x1
-#define MILEX_PROC 0x2
-#define MILEX_SMASK 0x3
+#define MILEX_OK   0x2
+#define MILEX_EOL  0x4
 #define MILEX_FAIL_INTERNAL 0x4
 #define MILEX_FAIL_PARSE    0x8
+/* types */
+#define MILEX_T_NONE   0x0
 #define MILEX_T_STRING 0x1
 #define MILEX_T_UINT   0x2
 #define MILEX_T_SINT   0x3
@@ -38,7 +40,7 @@ struct milex
 };
 
 /* \r, \n and bfsz == 0 match as EOL */
-void milex_next (struct milex *lex, char *buffer, size_t bfsz);
+void milex_next (struct milex * restrict lex, const char * restrict buffer, size_t bfsz);
 
 #endif /* _MILEX_1356959206_H_ */
 
